0518aa6d5484cc27213941f168f4931e4188d18f
PedroItaloQ <peedroitalo2906@gmail.com>
2024-05-30 13:10:58 -0300

    responsivityPokemonPage

--- src/pages/PokemonPage.tsx
+++ src/pages/PokemonPage.tsx
@@ -81,9 +81,16 @@ const PokemonPage: React.FC = () => {
                         </Box>
                         <Box display="flex" flexWrap="wrap">
                             {currentCards.map((pokemon, index) => (
-                                <Box key={index} width="16.666%" padding="10px">
-                                    <Card pokemon={pokemon} />
-                                </Box>
+                                 <Box 
+                                 key={index} 
+                                 width={{
+                                     base: "33.3333%", // 3 por linha em telas pequenas
+                                     md: "16.666%",    // 6 por linha em telas médias e maiores
+                                 }} 
+                                 padding="10px"
+                             >
+                                 <Card pokemon={pokemon} />
+                             </Box>
                             ))}
                         </Box>
                         <Box textAlign="center" marginTop="20px" display="flex" justifyContent="center">
